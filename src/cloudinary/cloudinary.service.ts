13a7d1ae3b53af9f97c9de9305fc908a075a6325
mohawwal <aanileleye@gmail.com>
2025-09-14 09:13:25 +0100

    multer

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -1,21 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { UploadApiErrorResponse, UploadApiResponse, v2 } from 'cloudinary';
-import toStream = require('buffer-to-stream');
+import { Readable } from 'stream';
 
 @Injectable()
 export class CloudinaryService {
   async uploadImage(file: Express.Multer.File): Promise<UploadApiResponse> {
     return new Promise((resolve, reject) => {
-      const upload = v2.uploader.upload_stream(
-        (error: UploadApiErrorResponse, result: UploadApiResponse) => {
-          if (error) return reject(error);
+      if (!file || !file.buffer) {
+        return reject(new Error('No file buffer provided'));
+      }
+
+      const uploadStream = v2.uploader.upload_stream(
+        (
+          error: UploadApiErrorResponse | undefined,
+          result?: UploadApiResponse,
+        ) => {
+          if (error) return reject(new Error(error.message));
           if (!result)
             return reject(new Error('Upload failed, no result returned.'));
           resolve(result);
         },
       );
 
-      toStream(file.buffer).pipe(upload);
+      const stream = Readable.from(file.buffer);
+      stream.pipe(uploadStream);
     });
   }
 }
