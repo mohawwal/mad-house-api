b9610bc33954a5fdf3d5096a260919e976670cd9
mohawwal <aanileleye@gmail.com>
2025-09-18 02:53:06 +0100

    fix: refactor CORS configuration to use a callback function for dynamic origin handling

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'http://localhost:8000',
-      'http://localhost:4000',
-      'https://admin-mad-house-vvxm.vercel.app',
-      'https://4-tl.vercel.app',
-    ],
+    origin: (
+      origin: string | undefined,
+      callback: (err: Error | null, allow?: boolean) => void,
+    ) => {
+      const allowedOrigins = [
+        'http://localhost:8000',
+        'http://localhost:4000',
+        'https://admin-mad-house-vvxm.vercel.app',
+        'https://4-tl.vercel.app',
+      ];
+
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
-  });
+  } as CorsOptions);
 
   app.use(cookieParser());
   app.setGlobalPrefix('api');
