1f50ad8ff4ff229bbb3bf3ade608dddb5f413e81
mohawwal <aanileleye@gmail.com>
2025-09-24 17:14:22 +0100

    update main

--- src/main.ts
+++ src/main.ts
@@ -14,8 +14,6 @@ async function bootstrap() {
       'https://4-tl.vercel.app',
     ],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
   });
 
   const port = process.env.PORT || 3000;
